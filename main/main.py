0dfa02bc2a5d642cbb51478730c5f6ee5be63aec
yumeno55 <novozhilov.2002@mail.ru>
2022-10-06 19:52:02 +0500

    new feature

--- main/main.py
+++ main/main.py
@@ -1,15 +1,25 @@
-def puzirek(a):
+def puzirek(a, obr = False):
     N = len(a)
-    i = 0
-    while i < N - 1:
-        j = 0
-        while j < N - 1 - i:
-            if a[j] > a[j + 1]:
-                a[j], a[j + 1] = a[j + 1], a[j]
-            j += 1
-        i += 1
+    if (obr == False):
+        i = 0
+        while i < N - 1:
+            j = 0
+            while j < N - 1 - i:
+                if a[j] > a[j + 1]:
+                    a[j], a[j + 1] = a[j + 1], a[j]
+                j += 1
+            i += 1
+    else:
+        i = 0
+        while i < N - 1:
+            j = 0
+            while j < N - 1 - i:
+                if a[j] < a[j + 1]:
+                    a[j], a[j + 1] = a[j + 1], a[j]
+                j += 1
+            i += 1
     return a
 
 
 a = [32, 45, 3, 89, 37, 22, 14, 5, 8]
-print(puzirek(a))
+print(puzirek(a, True))
